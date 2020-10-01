90cb824756932bce1b98c874ed33a98082fd975a
mueller <mueller@mbits.info>
2020-10-01 11:00:53 +0200

    Checkpoint commit as of 2020-10-01-11-00-52.

--- src/api.py
+++ src/api.py
@@ -6,7 +6,28 @@ import platform
 import shutil
 import subprocess
 import sys
+import tempfile
 from typing import List, Optional
+import subprocess
+
+import docx2pdf
+import lxml.etree as ET
+from docx import Document
+
+
+class Rule:
+    def __init__(self, name, concat_string="\n"):
+        self.name = name
+        self.concat_string = concat_string
+        self.xpath_expressions = []
+
+
+class Config:
+    def __init__(self, template_path, dsr2xml_exe="dsr2xml"):
+        self.template_path = template_path  # type: Optional[str]
+        self.dsr2xml_exe = dsr2xml_exe  # type: Optional[str]
+        self.rules = []  # type: List[Rule]
+
 
 def setup_logging(log_level=logging.INFO, log_file=None):
     class InfoFilter(logging.Filter):
@@ -32,229 +53,91 @@ def setup_logging(log_level=logging.INFO, log_file=None):
     logging.basicConfig(**kwargs)
 
 
-def sha256_sum(file_path):
-    h = hashlib.sha256()
-    b = bytearray(128 * 1024)
-    mv = memoryview(b)
-    with open(file_path, 'rb', buffering=0) as f:
-        for n in iter(lambda: f.readinto(mv), 0):
-            h.update(mv[:n])
-    return h.hexdigest()
+def run_cmd(*args):
+    logger = logging.getLogger(__name__)
+    cmd = ' '.join(args)
+    logger.debug("running the following command: {}".format(cmd))
+    result = subprocess.run(args, stdout=subprocess.PIPE)
+    if result.returncode != 0:
+        logger.error(
+            "cmd \"{}\" failed with code {} the following output: {}. aborting.".format(cmd, str(result.returncode),
+                                                                                        result.stderr))
+        sys.exit(-1)
+    else:
+        result = result.stdout.decode("utf-8")
+        if result:
+            logger.info(result)
 
+def replace_in_docx(docx_path, data, output_docx_path):
+    doc = Document(docx_path)
+    for i in data:
+        for p in doc.paragraphs:
+            if p.text.find(i) >= 0:
+                p.text = p.text.replace(i, data[i])
+    # save changed document
+    doc.save(output_docx_path)
 
-def get_int(str):
+def generate_report(dcm_sr_path, log_level, log_file):
+    # logging
+    setup_logging(log_level, log_file)
+    logger = logging.getLogger(__name__)
+
+    # files need to be deleted
     try:
-        value = int(str)
-        return value
-    except ValueError:
-        return str
+        rule = Rule("$findings$")
+        rule.xpath_expressions.append(
+            '/report/document/content/container/text[concept/meaning[contains(text(), "Finding")]]/value/text()')
+        config = Config(template_path="../sample_data/template.docx")
+        config.rules.append(rule)
 
+        # GENERATE XML FILE
+        with tempfile.NamedTemporaryFile(suffix=".xml", delete=log_level != logging.DEBUG) as tmp_file:
+            sr_xml_file = tmp_file.name
+        logger.info("converting DICOM SR {} to XML file {}".format(dcm_sr_path, sr_xml_file))
+        run_cmd(config.dsr2xml_exe, dcm_sr_path, sr_xml_file)
 
-class FileProcessor:
-    @abc.abstractmethod
-    def process(self, full_path, basename, file_name, ext, dir_name):
-        pass
-
-
-class TextProcessor:
-    @abc.abstractmethod
-    def process(self, text, full_path, basename, file_name, ext, dir_name):
-        pass
-
-
-class FileIndex:
-    @abc.abstractmethod
-    def is_indexed(self, full_path):
-        return False
-
-
-class DirParser:
-    def __init__(self, directory, file_processor, extensions=[]):
-        self.directory = directory  # type: Optional[str]
-        self.file_processor = file_processor  # type: FileProcessor
-        self.extensions = extensions  # type: List[Optional[str]]
-
-    def run(self):
-        for root, dirs, files in os.walk(self.directory):
-            for basename in files:
-                file_name, ext = os.path.splitext(basename)
-                ext = ext[1:].lower()
-                if len(self.extensions) > 0 and ext not in self.extensions:
-                    continue
-                path = os.path.join(root, basename)
-
-                self.file_processor.process(path, basename, file_name, ext, root)
-
-
-class TextractProcessor(FileProcessor):
-    def __init__(self, text_processor, index):
-        self.text_processor = text_processor  # type: TextProcessor
-        self.index = index  # type: FileIndex
-
-    def process(self, full_path, basename, file_name, ext, dir_name):
-        logger = logging.getLogger(__name__)
-        try:
-            if self.index.is_indexed(full_path):
-                logger.info("skipping already indexed file {}".format(full_path))
-                return
-
-            text = textract.process(full_path)
-            self.text_processor.process(text.decode('unicode_escape'), full_path, basename, file_name, ext, dir_name)
-        except Exception as e:
-            logger.warning("error processing file {}: {}".format(full_path, str(e)))
-
-
-class TesseractProcessor(FileProcessor):
-    def __init__(self, text_processor, index):
-        self.text_processor = text_processor  # type: TextProcessor
-        self.index = index  # type: FileIndex
-
-    def process(self, full_path, basename, file_name, ext, dir_name):
-        logger = logging.getLogger(__name__)
-        try:
-            if self.index.is_indexed(full_path):
-                logger.info("skipping already indexed file {}".format(full_path))
-                return
-
-            if ext == "pdf":
-                images = convert_from_path(full_path)
-            else:
-                images = [Image.open(full_path)]
-
+        # EXTRACT AND CONCAT CONTENTS USING XPATH
+        logger.info("retrieving contents from XML file {}".format(sr_xml_file))
+        root = ET.parse(sr_xml_file)
+        template_data = {}
+        for rule in config.rules:
             text = ""
-            for image in images:
-                text = text + pytesseract.image_to_string(image)
-        except Exception as e:
-            logger.warning("error processing file {}: {}".format(full_path, str(e)))
+            for xpath_expression in rule.xpath_expressions:
+                xpath_result = root.xpath(xpath_expression)
+                print(xpath_result)
+                if isinstance(xpath_result, List):
+                    xpath_result = rule.concat_string.join(xpath_result)
 
+                if xpath_result:
+                    if text:
+                        text = text + rule.concat_string
+                    text = text + xpath_result
+            template_data[rule.name] = text
+        logger.debug("template_data: {}".format(str(template_data)))
 
-class WhooshIndexer(TextProcessor):
-    def __init__(self, index_path, rebuild, commit_after_num_files=50):
-        self.index_path = index_path
-        self.rebuild = rebuild
-        self.commit_after_num_files = commit_after_num_files
-        # state
-        self.ix_writer = None
-        self.index_cleared = False
-        self.file_num = 0
+        # LOAD TEMPLATE AND SET CONTENTS ON NAMED PLACEHOLDERS
+        with tempfile.NamedTemporaryFile(suffix=".docx", delete=log_level != logging.DEBUG) as tmp_file:
+            docx_tmp_file = tmp_file.name
+        logger.info("replacing contents from template docx file {} into {}".format(config.template_path, docx_tmp_file))
+        replace_in_docx(config.template_path, template_data, docx_tmp_file)
 
-    def process(self, text, full_path, basename, file_name, ext, dir_name):
+        # CONVERT TO PDF
+        with tempfile.NamedTemporaryFile(suffix=".pdf", delete=log_level != logging.DEBUG) as tmp_file:
+            pdf_tmp_file = tmp_file.name
+        logger.info("converting file {} into pdf file {}".format(docx_tmp_file, pdf_tmp_file))
+        docx2pdf.convert(docx_tmp_file, pdf_tmp_file)
 
-        logger = logging.getLogger(__name__)
-        # sha256 = sha256_sum(full_path)
-        self.assert_index_writer()
-        logger.info("indexing file {}".format(full_path))
-        self.ix_writer.add_document(title=basename, path=full_path, file_id=full_path, content=text, textdata=text,
-                                    dir_name=dir_name)
-        self.file_num = self.file_num + 1
-        if self.commit_after_num_files is not None and self.file_num % self.commit_after_num_files == 0:
-            self.ix_writer.commit()
-            self.ix_writer = None
+        # CONVERT TO DICOM PDF
+        with tempfile.NamedTemporaryFile(suffix=".dcm", delete=log_level != logging.DEBUG) as tmp_file:
+            dcm_pdf_tmp_file = tmp_file.name
+        logger.info("converting file {} into pdf file {}".format(pdf_tmp_file, dcm_pdf_tmp_file))
+        run_cmd("pdf2dcm", pdf_tmp_file, dcm_pdf_tmp_file, "--series-from", dcm_sr_path)
 
-    def assert_index_writer(self):
+        # SEND TO DICOM NODE
+        logger.info("sending file {} to dicom node".format(dcm_pdf_tmp_file))
+        run_cmd("dcmsend", "localhost", "2727", dcm_sr_path)
+        run_cmd("dcmsend", "localhost", "2727", dcm_pdf_tmp_file)
 
-        logger = logging.getLogger(__name__)
-        if self.ix_writer is not None:
-            return
+    except Exception as error:
+        logger.exception(error)
 
-        if self.rebuild and os.path.exists(self.index_path) and self.index_cleared == False and exists_in(
-                self.index_path):
-            logger.info("deleting index at {}".format(self.index_path))
-            shutil.rmtree(self.index_path)
-            self.index_cleared = True
-
-        if os.path.exists(self.index_path):
-            self.ix_writer = open_dir(self.index_path).writer()
-        else:
-            os.makedirs(self.index_path, 0o777, True)
-            schema = Schema(title=TEXT(stored=True), path=TEXT(stored=True), dir_name=TEXT(stored=True),
-                            file_id=ID(stored=True), content=TEXT, textdata=TEXT(stored=True))
-            ix = create_in(self.index_path, schema)
-            self.ix_writer = ix.writer()
-
-    def __del__(self):
-        if self.ix_writer:
-            self.ix_writer.commit()
-
-
-class WooshIndex(FileIndex):
-    def __init__(self, index_path):
-        self.index_path = index_path
-        # state
-        self.indexed_paths = None
-
-    def is_indexed(self, full_path):
-        self.assert_indexed_paths()
-        return full_path in self.indexed_paths
-
-    def assert_indexed_paths(self):
-        if self.indexed_paths is not None:
-            return
-
-        self.indexed_paths = set()
-
-        if exists_in(self.index_path) == False:
-            return
-
-        ix = open_dir(self.index_path)
-        self.indexed_paths = set()
-        with ix.searcher() as searcher:
-            for fields in searcher.all_stored_fields():
-                indexed_path = fields['path']
-                self.indexed_paths.add(indexed_path)
-
-
-class DirOcr:
-
-    def index(self, directory, index_path, rebuild):
-        whoosh_indexer = WhooshIndexer(index_path, rebuild)
-        whoosh_index = WooshIndex(index_path)
-
-        file_processor = TextractProcessor(whoosh_indexer, whoosh_index)
-        extensions = []
-
-        dir_parser = DirParser(directory, file_processor, extensions)
-
-        dir_parser.run()
-
-    def interactive_search(self, index_path, num_docs):
-        while True:
-            query_str = input("query_string: ")
-            if query_str:
-                self.search(index_path, query_str, num_docs)
-
-    def open_file(self, full_path):
-        if platform.system() == 'Darwin':  # macOS
-            subprocess.call(('open', full_path))
-        elif platform.system() == 'Windows':  # Windows
-            os.startfile(full_path)
-        else:  # linux variants
-            subprocess.call(('xdg-open', full_path))
-
-    def search(self, index_path, query_str, num_docs):
-        ix = open_dir(index_path)
-
-        with ix.searcher(weighting=scoring.Frequency) as searcher:
-            query = QueryParser("content", ix.schema).parse(query_str)
-            results = searcher.search(query, limit=num_docs)
-
-            if len(results) == 0:
-                print("!!!No Results!!!")
-            else:
-                for i, result in enumerate(results):
-                    print("{}: {} in {}".format(i + 1, result['title'], result['dir_name']))
-
-                action = get_int(input("next search (ENTER), open file (<number>), open all files (a), quit(q): "))
-                if action:
-                    if action == "a":
-                        for result in results:
-                            self.open_file(result['path'])
-                    elif isinstance(action, int) and 0 < action <= len(results):
-                        self.open_file(results[action-1]['path'])
-                    elif action == "q":
-                        return
-
-    def __call__(self):
-        app = QApplication(sys.argv)
-        ex = App()
-        sys.exit(app.exec_())
--- src/report_generator.py
+++ src/report_generator.py
@@ -1,12 +1,15 @@
 import argparse
 import api
 import logging
+
 # args
-parser = argparse.ArgumentParser(description='Report Generator')
-parser.add_argument('--log_level', type=int, default=logging.INFO, help='the log level')
-parser.add_argument('--log_file', type=str, default=None, help='the log file')
+parser = argparse.ArgumentParser(
+    description='A utility to generate a nicely formatted DICOM PDF from a DICOM SR report using'
+                ' a template document (like word etc)')
+parser.add_argument('dicom_sr_file', type=str, help='actual DICOM SR report file')
+parser.add_argument('--log_level', type=int, default=logging.INFO,
+                    help='log level (CRITICAL = 50, ERROR = 40, WARNING = 30, INFO = 20, DEBUG = 10, NOTSET = 0')
+parser.add_argument('--log_file', type=str, default=None, help='log file')
 
 args = parser.parse_args()
-api.setup_logging(args.log_level, args.log_file)
-
-print("hello world")
+api.generate_report(args.dicom_sr_file, args.log_level, args.log_file)
